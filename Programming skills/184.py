9ee0eba517a80caa25c73600b8673333024e50ba
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2025-01-04 14:51:08 +0100

    184 - done

--- Programming skills/184.py
+++ Programming skills/184.py
@@ -3,32 +3,59 @@ import pandas as pd
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
     department.rename(columns={"name":"Department"}, inplace=True)
     employee.rename(columns={"name":"Employee", "salary":"Salary"}, inplace=True)
-    result_df:pd.DataFrame = pd.merge(employee, department, left_on="departmentId", right_on="id", how="outer")
+    employee_department_df:pd.DataFrame = pd.merge(employee, department, left_on="departmentId", right_on="id", how="left")
+
+    if len(employee_department_df) == 0:
+        return pd.DataFrame({ "Department": [], "Employee": [], "Salary": [] })
+
+    departments:list[str] = employee_department_df.loc[:,"Department"].unique().tolist()
     
-    # max_salaries = result_df.groupby(['Employee', 'Department'])['Salary'].idxmax()
-    print(f"\n-----------------------------------")
-    max_salary = result_df.groupby(['Department','Employee'])['Salary'].max(numeric_only=True)
-    # max_salary = max_salary.drop_duplicates(keep='first')
-    # print(f"max salaries:\n{max_salary}")
+    result_df:pd.DataFrame = pd.DataFrame()
 
-    max_salaries_per_dept = employee.groupby('departmentId')['Salary'].max().reset_index()
-    max_salaries = employee.groupby('departmentId')['Salary'].max().reset_index()['Salary'].to_list()
-    max_name = employee.groupby('departmentId')['Employee'].max().reset_index()['Employee'].to_list()
-    max_dept:pd.DataFrame = pd.merge(max_salaries_per_dept, department, left_on="departmentId", right_on="id", how="left")["Department"].to_list()
-    print(f"\n-----------------------------------")
-    mask_highest_salary = (result_df.loc[:,"Salary"].isin(max_salaries)) & (result_df.loc[:,"Department"].isin(max_dept)) & (result_df.loc[:,"Employee"].isin(max_name))
-    print(employee.groupby(['departmentId',"Employee"])['Salary'].max())
-    print(f"\n-----------------------------------")
+    for department in departments:
+        mask_department = (employee_department_df.loc[:,"Department"] == department)
+        max_salary:int = int(employee_department_df.loc[mask_department,["Salary"]].max().item())
+        mask_salary_department = (employee_department_df.loc[:,"Department"] == department) & (employee_department_df.loc[:,"Salary"] == max_salary)
 
-    # mask_max_salaries = result_df.loc[:,["Salary"]] == max_salaries
-    
-    # return result_df.loc[mask_max_salaries,['Department','Employee', 'Salary']]
-    return result_df.loc[mask_highest_salary,['Department','Employee','Salary']]
+        result_df = pd.concat([result_df, employee_department_df.loc[mask_salary_department,:]])
 
+    return result_df.loc[:,["Department", "Employee",  "Salary"]]
+
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 employee_data = { 'id': [1, 2, 3, 4, 5], 'name': ['Joe', 'Jim', 'Henry', 'Sam', 'Max'], 'salary': [70000, 90000, 80000, 60000, 90000], 'departmentId': [1, 1, 2, 2, 1] } 
 employee_df = pd.DataFrame(employee_data) 
 
 department_data = { 'id': [1, 2], 'name': ['IT', 'Sales'] } 
+department_df = pd.DataFrame(department_data)
+
+print(department_highest_salary(employee_df,department_df))
+
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+employee_data = data = { "id": [1, 2, 4], "name": ["Joe", "Sam", "Max"], "salary": [60000, 50000, 50000], "departmentId": [1, 1, 2] }
+employee_df = pd.DataFrame(employee_data) 
+
+
+data = { "Department": ["IT", "Sales", "IT"], "Employee": ["Jim", "Henry", "Max"], "Salary": [90000, 80000, 90000] }
+
+df = pd.DataFrame(data)
+print(df)
+
+print("________________________________________________________________________________________________________________________________________________________________")
+print(department_highest_salary(employee_df,department_df))
+print("________________________________________________________________________________________________________________________________________________________________")
+data = {
+    "Department": ["IT", "HR"],
+    "Employee": ["Joe", "Max"],
+    "Salary": [60000, 50000]
+}
+df = pd.DataFrame(data)
+print(df)
+
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+employee_data = { 'id': [], 'name': [], 'salary': [], 'departmentId': [] } 
+employee_df = pd.DataFrame(employee_data) 
+
+department_data = { 'id': [], 'name': [] } 
 department_df = pd.DataFrame(department_data)
 
 print(department_highest_salary(employee_df,department_df))
