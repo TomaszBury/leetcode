1ed404a50aba2bcc668ad40bc73e4faf4fa7ebbb
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2025-01-08 19:13:33 +0100

    1907- done

--- 30_days_pandas/1907.py
+++ 30_days_pandas/1907.py
@@ -1,31 +1,24 @@
 import pandas as pd
 
 def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+
+    mask_low_salary = (accounts.loc[:,"income"] < 20_000)
+    mask_average_salary = (accounts.loc[:,"income"] >= 20_000) & (accounts.loc[:,"income"] <= 50_000)
+    mask_high_salary = (accounts.loc[:,"income"] > 50_000)
+
+    low_salary:int = len(accounts.loc[mask_low_salary,"income"])
+    average_salary:int = len(accounts.loc[mask_average_salary,"income"])
+    high_salary:int = len(accounts.loc[mask_high_salary,"income"])
+
+
     categories = {
-        "Low Salary": 0,
-        "Average Salary": 0,
-        "High Salary": 0
+        "category": ["Low Salary", "Average Salary", "High Salary"],
+        "accounts_count":[low_salary,average_salary,high_salary]
+
     }
-
-    for account in accounts:
-        print(f"\nincome:{type(account)} account:{account}")
-        # income:int = int(account["income"])
-        # if income < 20000:
-        #     categories["Low Salary"] += 1
-        # elif 20000 <= income <= 50000:
-        #     categories["Average Salary"] += 1
-        # else:  # income > 50000
-        #     categories["High Salary"] += 1
-
-    # Here we format the results into the desired output structure
-    result = []
-    for category, count in categories.items():
-        result.append({
-            "category": category,
-            "accounts_count": count
-        })
+    result_df = pd.DataFrame(categories)
     
-    return result
+    return result_df
 
 
 
